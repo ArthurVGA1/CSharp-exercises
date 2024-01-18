be5ee7b6d2734d127b9ce8095c3cee3a804efeb9
Arthur Verissimo <arthurvga05@gmail.com>
2024-01-18 18:33:39 -0300

    feat: implementing simple calculator code

---
+++ 8_SimpleCalculator/Functions/Functions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator_CSharp.Functions
+{
+    internal class Functions
+    {
+        public double Sum(double x, double y)
+        {
+            double sum = x + y;
+
+            return sum;
+        }
+
+        public double Subtract(double x, double y)
+        {
+            double subtract = x - y;
+
+            return subtract;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            double multiply = x * y;
+
+            return multiply;
+        }
+
+        public double Divide(double x, double y)
+        {
+            double divide = x / y;
+
+            return divide;
+        }
+
+        public double Potency(int x, int y)
+        {
+            double pow = Math.Pow(x, y);
+
+            return pow;
+        }
+
+        public double SquareRoot(double x)
+        {
+            double sqrt = Math.Sqrt(x);
+
+            return sqrt;
+        }
+    }
+}
---
+++ 8_SimpleCalculator/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using SimpleCalculator_CSharp;
+using SimpleCalculator_CSharp.Functions;
+
+class Calculator
+{
+    static void Main(string[] args)
+    {
+        Functions functions = new Functions();
+
+        bool loop = true;
+        string option;
+
+        while (loop)
+        {
+            Console.Clear();
+            Console.WriteLine("Write 2 numbers:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose one of the operations:\n+ - Sum.\n- - Subtract.\n* - Multiply.\n/ - Divide.\n^ - Potency.\n/2- Square root.\n0 - Back.\n00 - Close.");
+            option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "+":
+                    Console.WriteLine(functions.Sum(x, y));
+                    Console.ReadLine();
+                    break;
+
+                case "-":
+                    Console.WriteLine(functions.Subtract(x, y));
+                    Console.ReadLine();
+                    break; 
+
+                case "*":
+                    Console.WriteLine(functions.Multiply(x, y));
+                    Console.ReadLine();
+                    break;
+
+                case "/":
+                    Console.WriteLine(functions.Divide(x, y));
+                    Console.ReadLine();
+                    break;
+
+                case "^":
+                    Console.WriteLine(functions.Potency(x, y));
+                    Console.ReadLine();
+                    break;
+
+                case "/2":
+                    Console.WriteLine(functions.SquareRoot(x));
+                    Console.ReadLine();
+                    break;
+
+                case "0":
+                    loop = true;
+                    break;
+
+                case "00":
+                    loop = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option.");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+}
